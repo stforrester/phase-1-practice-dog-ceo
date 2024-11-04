35b3b1efefb63764773e48ad1fbb5af40e9b005b
Stuart Forrester <stforrester@gmail.com>
2024-11-04 16:47:17 -0600

    Accomplishes Challenge 1 of the lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+
+// on page load, fetches the images using the url above ⬆️
+function fetchFunction() {
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then((data)=>{
+     data.message.forEach((element)=>{
+         let img = document.createElement("img")
+         let div = document.getElementById("dog-image-container")
+         img.src = `${element}`
+         div.appendChild(img)
+         console.log(div)
+     })
+    })
+} 
+
+// fetchFunction()
+
+document.addEventListener("DOMContentLoaded", fetchFunction)
+// // parses the response as JSON
+// adds image elements to the DOM for each 🤔 image in the array
